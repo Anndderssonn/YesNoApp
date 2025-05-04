3f693ca9fd143edfc6356ec41459ea81d74b966e
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-05-03 21:19:02 -0500

    feat: Add provider module

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> messages = [
+    Message(text: 'Hi darling', fromWho: FromWho.me),
+    Message(text: 'Are you at home?', fromWho: FromWho.me),
+  ];
+
+  Future<void> sendMessage(String text) async {
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messages.add(newMessage);
+    notifyListeners();
+  }
+}
